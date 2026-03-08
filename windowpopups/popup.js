d13d93b1a594008565c6ad171e04d46e96d085b8
GabyH64 <mc.cyan.gabyh@gmail.com>
2026-03-08 12:04:38 -0600

    window popups v3

--- windowpopups/popup.js
+++ windowpopups/popup.js
@@ -113,9 +113,104 @@ document.addEventListener('DOMContentLoaded', function () {
             url: '10pop.html',
             type: 'popup',
             width: 250,
-            height: 285,
-            left: 140,
-            top: 25
+            height: 180,
+            left: 180,
+            top: 15
+
+        });
+        }
+
+        function popup11(){ //i know the
+        chrome.windows.create({
+            url: '11pop.html',
+            type: 'popup',
+            width: 250,
+            height: 100,
+            left: 220,
+            top: 35
+
+        });
+        }
+
+        function popup11a(){ // the
+        chrome.windows.create({
+            url: '11pop2.html',
+            type: 'popup',
+            width: 80,
+            height: 90,
+            left: 220,
+            top: 120
+
+        });
+        }
+
+        function popup12(){ //rest
+        chrome.windows.create({
+            url: '12pop.html',
+            type: 'popup',
+            width: 250,
+            height: 90,
+            left: 300,
+            top: 120
+
+        });
+        }
+
+        function popup13(){ //it's good
+        chrome.windows.create({
+            url: '13pop.html',
+            type: 'popup',
+            width: 250,
+            height: 90,
+            left: 500,
+            top: 20
+
+        });
+        }
+
+        function popup14(){ //I
+        chrome.windows.create({
+            url: '14pop.html',
+            type: 'popup',
+            width: 250,
+            height: 90,
+            left: 500,
+            top: 100
+
+        });
+        }
+
+        function popup15(){ //guess
+        chrome.windows.create({
+            url: '15pop.html',
+            type: 'popup',
+            width: 250,
+            height: 90,
+            left: 600,
+            top: 100
+
+        });
+        }
+        function popup16(){ //the cavern
+        chrome.windows.create({
+            url: '16pop.html',
+            type: 'popup',
+            width: 250,
+            height: 90,
+            left: 700,
+            top: 20
+
+        });
+        }
+
+        function popup17(){ //is a crowd
+        chrome.windows.create({
+            url: '17pop.html',
+            type: 'popup',
+            width: 250,
+            height: 90,
+            left: 700,
+            top: 100
 
         });
         }
@@ -128,8 +223,16 @@ document.addEventListener('DOMContentLoaded', function () {
     setTimeout(popup6, 3500);
     setTimeout(popup7, 3500); //i camein
     setTimeout(popup8, 4300);//a bit before
-    setTimeout(popup9, 5000); //theopening set
-    setTimeout(popup10, 6000); //i tookthebet
+    setTimeout(popup9, 5100); //theopening set
+    setTimeout(popup10, 6300); //i tookthebet
+    setTimeout(popup11, 7300); //i know 
+    setTimeout(popup11a, 7900); //the
+    setTimeout(popup12, 8200); //rest   
+    setTimeout(popup13, 8600); //it5's good
+    setTimeout(popup14, 9200); //I
+    setTimeout(popup15, 9500); //guess
+    setTimeout(popup16, 10000); //the cavern
+    setTimeout(popup17, 10500); //is a crowd
     
     
 });
